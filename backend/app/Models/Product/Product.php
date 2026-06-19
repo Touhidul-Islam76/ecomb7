63e50c6ca4f631da8a1d16a575e26c42e713203b
Touhidul Islam <mdtouhidul426@gmail.com>
2026-06-20 01:47:23 +0600

    applied relation to product and added scope filtering

--- backend/app/Models/Product/Product.php
+++ backend/app/Models/Product/Product.php
@@ -2,6 +2,8 @@
 
 namespace App\Models\Product;
 
+use App\Models\Brand\Brand;
+use App\Models\Category\Category;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
@@ -21,4 +23,24 @@ class Product extends Model
         'star',
         'remarks',
     ];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+    public function brand(){
+        return $this->belongsTo(Brand::class);
+    }
+
+    //scope query for filtering
+    public function scopeProductFilter($query, $request){
+        if($request->filled('category_id')){
+            $query->where('category_id', $request->category_id);
+        }
+        if($request->filled('brand_id')){
+            $query->where('brand_id', $request->brand_id);
+        }
+        if($request->filled('remarks')){
+            $query->where('remarks', $request->remarks);
+        }
+    }
 }
