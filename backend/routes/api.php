7aacb59a2072a36d2756bad5e2725ac50d9a00a9
Touhidul Islam <mdtouhidul426@gmail.com>
2026-06-20 01:46:19 +0600

    Update api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\Brand\BrandController;
+use App\Http\Controllers\Category\CategoryController;
+use App\Http\Controllers\Product\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::group(['prefix' => 'v1'], function(){
+    Route::get('brands', [BrandController::class, 'index']);
+    Route::get('categories', [CategoryController::class, 'index']);
+    Route::get('products', [ProductController::class, 'index']);
+});
