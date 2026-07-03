ff5e4d5038e69322857f4fe2ecb5952543fd4c95
Touhidul Islam <mdtouhidul426@gmail.com>
2026-07-04 00:56:37 +0600

    add and delete wishlist controller created

--- backend/app/Http/Controllers/Product/UserProductController.php
+++ backend/app/Http/Controllers/Product/UserProductController.php
@@ -3,9 +3,39 @@
 namespace App\Http\Controllers\Product;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Product\User\Wishlist\WishlistRequest;
+use App\Models\Product\ProductWishList;
+use GrahamCampbell\ResultType\Success;
 use Illuminate\Http\Request;
 
 class UserProductController extends Controller
 {
-    //
+    public function addWishlist(WishlistRequest $req){
+        $user = auth()->user();
+        if(ProductWishList::where('user_id', $req->user_id)->where('product_id', $req->product_id)->exists()){
+            return $this->error(['Product already added to the wishlist']);
+        }
+
+        ProductWishList::create([
+            'user_id' => $user->id,
+            'product_id'=> $req->product_id
+        ]);
+
+        return $this->success(null, ['Product added to the wishlist']);
+
+
+    }
+
+    public function deleteWishlist( WishlistRequest $req ){
+        $user = auth()->user();
+
+        $productWishliSt = ProductWishList::where('user_id', $req->user_id)->where('product_id', $req->product_id)->first();
+
+        if(!$productWishliSt){
+            return $this->error(['This product is not in your wishlist']);
+        }
+
+        $productWishliSt->delete();
+        return $this->success(null, ['Product Successfully removed from your wishlist']);
+    }
 }
