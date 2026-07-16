ec756d9ca09e055678f9c303fc670f13a9898d8e
Touhidul Islam <mdtouhidul426@gmail.com>
2026-07-16 21:54:21 +0600

    used pinia and router globally

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import { createPinia } from 'pinia'; //imported from pinia after installation
+import router from '@/router'
 
-createApp(App).mount('#app')
+
+
+// using pinia by use(createPinia())
+createApp(App).use(createPinia()).use(router).mount('#app');
