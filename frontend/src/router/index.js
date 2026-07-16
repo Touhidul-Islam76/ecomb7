1973a11654c9cae53c09880b9c48ee1eb5f6c1d2
Touhidul Islam <mdtouhidul426@gmail.com>
2026-07-16 22:54:40 +0600

    router setup for route link completed

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,1 +1,25 @@
-import { createRouter,createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
+import Login from "../views/Auth/Login.vue";
+import Profile from "../views/Profile.vue";
+
+
+
+// defining the routes where components will be shown
+const routes = [
+  {
+    path: "/login",
+    component: Login,
+  },
+  {
+    path: "/profile",
+    component: Profile,
+  },
+];
+
+
+const router = createRouter({
+    history:createWebHistory,
+    routes
+});
+
+export default router;
