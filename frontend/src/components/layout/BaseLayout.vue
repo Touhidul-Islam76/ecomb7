d884db2ab1e6ed177771ffb6b284869a8e6a973d
Touhidul Islam <mdtouhidul426@gmail.com>
2026-07-18 23:24:33 +0600

    base design created

---
+++ frontend/src/components/layout/BaseLayout.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <Header></Header>
+        <!-- Dynamic contents will be visualize here by using slot -->
+            <slot></slot>
+        <Footer></Footer>
+    </div>
+</template>
+
+<script setup >
+    import Header from './Header.vue';
+    import Footer from './Footer.vue';
+</script>
