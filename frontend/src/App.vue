7b9083a81d47bb15c88575b5557379a5a6fd83d8
Touhidul Islam <mdtouhidul426@gmail.com>
2026-07-18 23:24:41 +0600

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,13 @@
 <script setup>
+import BaseLayout from './components/layout/BaseLayout.vue';
+
 
 </script>
 
 <template>
-  <h1>Home page</h1>
-  <RouterView></RouterView>
+  <!-- Dynamic pages will be shown here using RouterView -->
+   <BaseLayout>
+      <RouterView></RouterView>
+  </BaseLayout>
+
 </template>
