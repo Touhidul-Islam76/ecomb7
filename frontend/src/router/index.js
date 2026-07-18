a6158340bc39238e1b3fead7aac4b7a1c391e316
Touhidul Islam <mdtouhidul426@gmail.com>
2026-07-18 23:23:36 +0600

    home, account route added

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,18 +1,27 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "../views/Auth/Login.vue";
 import Profile from "../views/Profile.vue";
-
+import Home from "../views/Home.vue";
+import MyAccount from "../views/dashboard/MyAccount.vue";
 
 
 // defining the routes where components will be shown
 const routes = [
   {
+    path: "/",
+    component: Home,
+  },
+    {
     path: "/login",
     component: Login,
   },
   {
     path: "/profile",
     component: Profile,
+  },
+    {
+    path: "/dashboard/my-account",
+    component: MyAccount,
   },
 ];
 
