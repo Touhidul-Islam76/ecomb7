be480be3168b12dc8af92a21adccca729e3e4f96
Touhidul Islam <mdtouhidul426@gmail.com>
2026-08-05 14:38:14 +0600

    Update Header.vue

--- frontend/src/components/layout/Header.vue
+++ frontend/src/components/layout/Header.vue
@@ -383,30 +383,27 @@
                         </li>
                         <li class="dropdown cart_dropdown"><a class="nav-link cart_trigger" href="#"
                                 data-bs-toggle="dropdown"><i class="linearicons-cart"></i><span
-                                    class="cart_count">2</span></a>
-                            <div class="cart_box dropdown-menu dropdown-menu-right">
-                                <ul class="cart_list">
+                                    class="cart_count">{{ cartStores.count }}</span></a>
+                            <div class="cart_box dropdown-menu dropdown-menu-right"
+                                style="max-height: 350px; overflow-y: auto;">
+                                <ul class="cart_list" v-for="cart in cartStores.allCart" :key="cart.id">
                                     <li>
-                                        <a href="#" class="item_remove"><i class="ion-close"></i></a>
-                                        <a href="#"><img src="/assets/images/cart_thamb1.jpg" alt="cart_thumb1">Variable
-                                            product 001</a>
-                                        <span class="cart_quantity"> 1 x <span class="cart_amount"> <span
-                                                    class="price_symbole">$</span></span>78.00</span>
+                                        <a href="#" @click.prevent="remove(cart.id)" class="item_remove"><i
+                                                class="ion-close"></i></a>
+                                        <a href="#"><img src="/assets/images/cart_thamb1.jpg" alt="cart_thumb1">{{
+                                            cart.product.title }}</a>
+                                        <span class="cart_quantity"> {{ cart.quantity }} x <span class="cart_amount">
+                                                <span class="price_symbole">$</span></span>{{ cart.product.price
+                                            }}</span>
                                     </li>
-                                    <li>
-                                        <a href="#" class="item_remove"><i class="ion-close"></i></a>
-                                        <a href="#"><img src="/assets/images/cart_thamb2.jpg" alt="cart_thumb2">Ornare
-                                            sed consequat</a>
-                                        <span class="cart_quantity"> 1 x <span class="cart_amount"> <span
-                                                    class="price_symbole">$</span></span>81.00</span>
-                                    </li>
+
                                 </ul>
                                 <div class="cart_footer">
                                     <p class="cart_total"><strong>Subtotal:</strong> <span class="cart_price"> <span
-                                                class="price_symbole">$</span></span>159.00</p>
-                                    <p class="cart_buttons"><a href="#"
-                                            class="btn btn-fill-line rounded-0 view-cart">View Cart</a><a href="#"
-                                            class="btn btn-fill-out rounded-0 checkout">Checkout</a></p>
+                                                class="price_symbole">$</span></span>{{ (cartStores.subtotal).toFixed(2) }}</p>
+                                    <p class="cart_buttons"><router-link to="/carts"
+                                            class="btn btn-fill-line rounded-0 view-cart">View Cart</router-link><a
+                                            href="#" class="btn btn-fill-out rounded-0 checkout">Checkout</a></p>
                                 </div>
                             </div>
                         </li>
@@ -420,11 +417,40 @@
 </template>
 
 <script setup>
+import { onMounted, ref } from 'vue';
 import { useAuth } from '../../stores/auth';
+import { cartStore } from '../../stores/Product/cartStore';
 import Toastify from 'toastify-js';
+import http from '../../library/http';
 
 const auth = useAuth();
+const cartStores = cartStore();
+const cartList = ref([]);
 // const message = auth.message;
+
+onMounted(async () => {
+    await cartStores.allCarts();
+    // cartList.value = res.value;
+    console.log("cart data from header:", cartStores.allCart);
+})
+
+const remove = async (product_id) => {
+    const res = await http.post('cart/delete', { cart_id: product_id })
+    if (res) {
+        Toastify({
+
+            text: res.data.massage[0],
+
+            duration: 3000
+
+        }).showToast();
+
+        setInterval(()=>{
+            window.location.reload();
+        },300)
+    }
+}
+
 const logout = () => {
     auth.logout();
     Toastify({
