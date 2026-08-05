a5e729360bd63e59673894e1dcd41f619d8273d3
Touhidul Islam <mdtouhidul426@gmail.com>
2026-08-05 14:38:50 +0600

    added protected cart page route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,7 +5,7 @@ import Home from "../views/Home.vue";
 import MyAccount from "../views/dashboard/MyAccount.vue";
 import { useAuth } from "../stores/auth.js";
 import Wishlist from "../views/dashboard/Wishlist.vue";
-
+import Carts from "../views/dashboard/Carts.vue";
 
 // defining the routes where components will be shown
 const routes = [
@@ -13,40 +13,44 @@ const routes = [
     path: "/",
     component: Home,
   },
-    {
+  {
     path: "/login",
-    name:'login',
+    name: "login",
     component: Login,
   },
   {
-    path:"/wishlist",
+    path: "/wishlist",
     component: Wishlist,
-    // this meta:{ requireAuth:true } is for that page which is secured and accesable only for the logged in users  
-    meta: { requireAuth:true }
+    // this meta:{ requireAuth:true } is for that page which is secured and accesable only for the logged in users
+    meta: { requireAuth: true },
+  },
+  {
+    path: "/carts",
+    component: Carts,
+    // this meta:{ requireAuth:true } is for that page which is secured and accesable only for the logged in users
+    meta: { requireAuth: true },
   },
   {
     path: "/profile",
     component: Profile,
   },
-    {
+  {
     path: "/dashboard/my-account",
     component: MyAccount,
-    // this meta:{ requireAuth:true } is for that page which is secured and accesable only for the logged in users  
-    meta: { requireAuth:true }
+    // this meta:{ requireAuth:true } is for that page which is secured and accesable only for the logged in users
+    meta: { requireAuth: true },
   },
 ];
 
-
 const router = createRouter({
-    history:createWebHistory(),
-    routes
+  history: createWebHistory(),
+  routes,
 });
 
-
-// here we are redirecting the user who is not logged in to the login page and not giving permission to "/dashboard/my-account" page 
-router.beforeEach( (to)=>{
+// here we are redirecting the user who is not logged in to the login page and not giving permission to "/dashboard/my-account" page
+router.beforeEach((to) => {
   const auth = useAuth();
-  if(to.meta.requireAuth && !auth.isAuthenticated) return { name: 'login'};
-} );
+  if (to.meta.requireAuth && !auth.isAuthenticated) return { name: "login" };
+});
 
 export default router;
