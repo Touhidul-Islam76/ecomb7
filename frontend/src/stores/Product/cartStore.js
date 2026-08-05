8e0339c49dce5f4156cf9dd9e5b60056df8618b4
Touhidul Islam <mdtouhidul426@gmail.com>
2026-08-05 15:37:45 +0600

    Update cartStore.js

--- frontend/src/stores/Product/cartStore.js
+++ frontend/src/stores/Product/cartStore.js
@@ -10,8 +10,8 @@ export const cartStore = defineStore("cartStore", {
   }),
   getters: {
     is_authenticated: (check) => !!check.access_token,
-    count: (cart)=> cart.allCart ? cart.allCart.length : [],
-    subtotal: (cart)=> cart.allCart.reduce((sum, i)=> sum + i.quantity*i.price, 0)
+    count: (cart)=> cart.allCart ? cart.allCart.length : 0,
+    subtotal: (cart)=> cart.allCart && cart.allCart.length ? cart.allCart.reduce((sum, i)=> sum + i.quantity*i.price, 0) : 0
   },
   actions: {
     async addCart(product) {
@@ -19,18 +19,20 @@ export const cartStore = defineStore("cartStore", {
         const res = await http.post("cart/add", {
           product_id: product,
           quantity: 1,
+          color: "white",
           size: "m",
-          size: "white",
         });
 
         if (res && this.is_authenticated) {
-          // this.allCart = res.data.data
           console.log(res);
           Toastify({
-            text: res.data.massage[0],
-
+            text: res.data.massage ? res.data.massage[0] : 'Added to cart',
             duration: 1000,
           }).showToast();
+
+          // refresh local cart state so UI updates immediately
+          await this.allCarts();
+          return this.allCart;
         }
       } catch (e) {
         this.message = "Something went wrong!!!";
@@ -40,8 +42,9 @@ export const cartStore = defineStore("cartStore", {
       try {
         const res = await http.get("/cart");
         if (res) {
-          ((this.allCart = res.data.data),
-            console.log("fetched cart:", res.data.data));
+          this.allCart = res.data.data;
+          console.log("fetched cart:", res.data.data);
+          return this.allCart;
         }
       } catch (e) {
         this.message = "SOmething went wrong";
