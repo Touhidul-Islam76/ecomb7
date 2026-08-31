a431312a9027d736bae0c14e258f260e5f905841
Touhidul Islam <mdtouhidul426@gmail.com>
2026-09-01 01:28:53 +0600

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -8,6 +8,11 @@ import Wishlist from "../views/dashboard/Wishlist.vue";
 import Carts from "../views/dashboard/Carts.vue";
 import AdminLogin from "../views/Admin/AdminLogin.vue";
 import AdminDashboard from "../views/Admin/AdminDashboard.vue";
+import Orders from "../views/Admin/Orders.vue";
+import Users from "../views/Admin/Users.vue";
+import Categories from "../views/Admin/Categories.vue";
+import Products from "../views/Admin/Products.vue";
+import Settings from "../views/Admin/Settings.vue";
 
 // defining the routes where components will be shown
 const routes = [
@@ -48,6 +53,30 @@ const routes = [
   },{
     path: "/admin/dashboard-acc",
     component: AdminDashboard,
+    meta: { requireAuth: true },
+  },
+  {
+    path: "/admin/orders",
+    component: Orders,
+    meta: { requireAuth: true },
+  },
+  {
+    path:"/admin/users",
+    component: Users,
+    meta: { requireAuth: true },
+  },
+  {
+    path:"/admin/categories",
+    component: Categories,
+    meta: { requireAuth: true },
+  },
+  {
+    path:"/admin/products",
+    component: Products,
+    meta: { requireAuth: true },
+  },{
+    path:"/admin/settings",
+    component: Settings,
     meta: { requireAuth: true },
   }
 ];
