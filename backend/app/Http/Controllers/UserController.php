52431bebacff56f9969107ca55def794cdb4c9a6
Touhidul Islam <mdtouhidul426@gmail.com>
2026-09-04 19:56:39 +0600

    Update UserController.php

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\Auth\CreateRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Spatie\Permission\Models\Role;
 
 class UserController extends Controller
 {
@@ -30,7 +31,7 @@ class UserController extends Controller
                 'password' => $request->password,
                 'permissions' => $user->getAllPermissions()->pluck('name'),
             ]
-        ]);
+        ], ['User created successfully']);
     }
 
     public function me( Request $request ){
@@ -48,5 +49,42 @@ class UserController extends Controller
 
     }
 
-    public function index(){}
+    public function index(){
+        $user = User::latest()->get();
+        if(!$user){
+            return $this->error(['User not found'], 404);
+        }else{
+            return $this->success($user, ['Users retrieved successfully']);
+        }
+    }
+
+    public function allRoles(){
+        $roles = Role::get();
+        if(!$roles){
+            return $this->error(['Roles not found'], 404);
+        }else{
+            return $this->success($roles, ['Roles retrieved successfully']);
+        }
+    }
+
+    public function update(Request $req, User $user){
+        $user->update([
+            'name' => $req->name ?? $user->name,
+            'email' => $req->email ?? $user->email,
+            'password' => $req->password ?? $user->password,
+        ]);
+
+        if($req->role){
+            $user->syncRoles($req->role);
+        }
+
+        return $this->success([
+            'user' => $this->formatUser($user)
+        ], ['User updated successfully']);
+    }
+
+    public function destroy(User $user){
+        $user->delete();
+        return $this->success(null, ['User deleted successfully']);
+    }
 }
